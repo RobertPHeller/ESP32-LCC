0ec25d149692614ac9794fe2ce8a6b0078ffebfc
Robert Heller <heller@deepsoft.com>
2023-01-15 16:53:00 -0500

    Created firmware for esp32octalservo.

---
+++ ESP32-OctalServoOpenMRNIDF/components/Config/include/cdi.hxx
@@ -0,0 +1,108 @@
+// -!- c++ -!- //////////////////////////////////////////////////////////////
+//
+//  System        : 
+//  Module        : 
+//  Object Name   : $RCSfile$
+//  Revision      : $Revision$
+//  Date          : $Date$
+//  Author        : $Author$
+//  Created By    : Robert Heller
+//  Created       : Sat Jun 25 09:33:40 2022
+//  Last Modified : <230115.1644>
+//
+//  Description	
+//
+//  Notes
+//
+//  History
+//	
+/////////////////////////////////////////////////////////////////////////////
+//
+//    Copyright (C) 2022  Robert Heller D/B/A Deepwoods Software
+//			51 Locke Hill Road
+//			Wendell, MA 01379-9728
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// 
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef __CDI_HXX
+#define __CDI_HXX
+
+#include "NodeIdConfigurationGroup.hxx"
+#include "freertos_drivers/esp32/Esp32WiFiConfiguration.hxx"
+#include "WiFiConfigurationGroup.hxx"
+#include <openlcb/ConfigRepresentation.hxx>
+#include "openlcb/ConfiguredConsumer.hxx"
+#include "openlcb/ConfiguredProducer.hxx"
+#include "openlcb/MemoryConfig.hxx"
+#include "openlcb/ServoConsumerConfig.hxx"
+
+namespace esp32octalservo
+{
+
+constexpr uint8_t SERVOCOUNT = 8;
+using ServoConfigGroup = openlcb::RepeatedGroup<openlcb::ServoConsumerConfig, SERVOCOUNT>;
+
+
+/// Defines the main segment in the configuration CDI. This is laid out at
+/// origin 128 to give space for the ACDI user data at the beginning.
+CDI_GROUP(IoBoard, Segment(openlcb::MemoryConfigDefs::SPACE_CONFIG),
+          Offset(128));
+/// Each entry declares the name of the current entry, then the type and then
+/// optional arguments list.
+CDI_GROUP_ENTRY(internal_config, openlcb::InternalConfigData);
+CDI_GROUP_ENTRY(servos, ServoConfigGroup, Name("Servos"), RepName("Servo"));
+#ifdef CONFIG_ESP32_WIFI_ENABLED
+CDI_GROUP_ENTRY(olbcwifi, openmrn_arduino::WiFiConfiguration, Name("OLBC WiFi Configuration"));
+#endif
+CDI_GROUP_END();
+
+/// This segment is only needed temporarily until there is program code to set
+/// the ACDI user data version byte.
+CDI_GROUP(VersionSeg, Segment(openlcb::MemoryConfigDefs::SPACE_CONFIG),
+    Name("Version information"));
+CDI_GROUP_ENTRY(acdi_user_version, openlcb::Uint8ConfigEntry,
+    Name("ACDI User Data version"), Description("Set to 2 and do not change."));
+CDI_GROUP_END();
+
+/// The main structure of the CDI. ConfigDef is the symbol we use in main.cxx
+/// to refer to the configuration defined here.
+CDI_GROUP(ConfigDef, MainCdi());
+/// Adds the <identification> tag with the values from SNIP_STATIC_DATA above.
+CDI_GROUP_ENTRY(ident, openlcb::Identification);
+/// Adds an <acdi> tag.
+CDI_GROUP_ENTRY(acdi, openlcb::Acdi);
+/// Adds a segment for changing the values in the ACDI user-defined
+/// space. UserInfoSegment is defined in the system header.
+CDI_GROUP_ENTRY(userinfo, openlcb::UserInfoSegment, Name("User Info"));
+/// Adds the main configuration segment.
+CDI_GROUP_ENTRY(seg, IoBoard, Name("Board Configuration"));
+CDI_GROUP_ENTRY(node, NodeIdConfig, Name("Node ID"));
+#if defined(CONFIG_ESP32_WIFI_ENABLED)
+CDI_GROUP_ENTRY(wifi, WiFiConfiguration, Name("WiFi Configuration"));
+#endif
+/// Adds the versioning segment.
+CDI_GROUP_ENTRY(version, VersionSeg);
+CDI_GROUP_END();
+
+
+
+}
+
+#endif // __CDI_HXX
+

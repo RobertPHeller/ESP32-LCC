873484aea1c5bdcb5de4793e2bd9a8f17538d00d
Robert Heller <heller@deepsoft.com>
2023-02-21 16:04:17 -0500

    Convert ESP32-OctalServoOpenMRNIDF to use shared NVS component.

--- ESP32-OctalServoOpenMRNIDF/main/esp32octalservo.cpp
+++ ESP32-OctalServoOpenMRNIDF/main/esp32octalservo.cpp
@@ -8,7 +8,7 @@
 //  Author        : $Author$
 //  Created By    : Robert Heller
 //  Created       : Thu Jun 23 12:17:40 2022
-//  Last Modified : <230206.0942>
+//  Last Modified : <230221.1554>
 //
 //  Description	
 //
@@ -163,7 +163,7 @@ ConfigUpdateListener::UpdateAction FactoryResetHelper::apply_configuration(
     AutoNotify n(done);
     LOG(VERBOSE, "[CFG] apply_configuration(%d, %d)", fd, initial_load);
     if (!initial_load &&
-        Singleton<NvsManager>::instance()->CheckPersist())
+        Singleton<nvsmanager::NvsManager>::instance()->CheckPersist())
     {
         LOG(WARNING, "[CFG] NVS has been updated requesting a restart.");
         return ConfigUpdateListener::UpdateAction::REBOOT_NEEDED;
@@ -206,12 +206,12 @@ void app_main()
     
     //GpioInit::hw_init();
     
-    esp32octalservo::NvsManager nvs;
+    nvsmanager::NvsManager nvs;
     nvs.init(reset_reason);
     
     LOG(INFO, "[BootPauseHelper] starting...");
     
-    esp32octalservo::BootPauseHelper pause;
+    nvsmanager::BootPauseHelper pause;
     
     pause.CheckPause();
     LOG(INFO, "[BootPauseHelper] returned...");

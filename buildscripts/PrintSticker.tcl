48b9e56232f0162a46e87f97f8b88d411fa356b1
Robert Heller <heller@deepsoft.com>
2023-03-09 12:33:49 -0500

    Complete PrintSticker.

--- buildscripts/PrintSticker.tcl
+++ buildscripts/PrintSticker.tcl
@@ -8,7 +8,7 @@
 #  Author        : $Author$
 #  Created By    : Robert Heller
 #  Created       : Fri Mar 3 16:40:36 2023
-#  Last Modified : <230303.1702>
+#  Last Modified : <230309.1209>
 #
 #  Description	
 #
@@ -62,8 +62,31 @@ snit::type PrintSticker {
         set _openmrngitcommit [gets $fp]
         set _buildflags [gets $fp]
         close $fp
-        set _printer [POSReceiptPrinter create %AUTO% /dev/usb/lp2]
-        
+        set _printer [POSReceiptPrinter create %AUTO% /dev/LABELS]
+        $_printer selectCenterJustification
+        $_printer _doubleStrikeOn
+        $_printer textLine $_builddate
+        $_printer _doubleStrikeOff
+        $_printer selectLeftJustification
+        $_printer selectFontB
+        if {[regexp {^b'([[:xdigit:]]+)':(.*)$} $_gitcommit => commit remainder] > 0} {
+            set remainder [regsub {:-d} $remainder {}]
+            set _gitcommit "$commit:$remainder"
+        }
+        $_printer textLine $_gitcommit
+        if {[regexp {^([[:xdigit:]]+):(.*)$} $_openmrngitcommit => commit what] > 0} {
+            set _openmrngitcommit "[string range $commit 0 6]:$what"
+        }
+        $_printer textLine $_openmrngitcommit
+        $_printer selectFontA
+        set _flags [list]
+        foreach f $_buildflags {
+            lassign [split $f {=}] flag value
+            if {$value eq {""}} {continue}
+            lappend _flags $flag
+        }
+        $_printer selectCenterJustification
+        $_printer textLine "Flags: $_flags"
         $_printer destroy
     }
 }

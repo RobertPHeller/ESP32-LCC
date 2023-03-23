4d5d279a6fa98537915daea11812e8bca8792968
Robert Heller <heller@deepsoft.com>
2023-03-23 17:04:54 -0400

    Working on program documentation.

--- CommonComponents/GeneralIO/include/Turnout.hxx
+++ CommonComponents/GeneralIO/include/Turnout.hxx
@@ -1,5 +1,44 @@
-/**
- */
+// -!- C++ -!- //////////////////////////////////////////////////////////////
+//
+//  System        : 
+//  Module        : 
+//  Object Name   : $RCSfile$
+//  Revision      : $Revision$
+//  Date          : $Date$
+//  Author        : $Author$
+//  Created By    : Robert Heller
+//  Created       : Sun Feb 24 14:51:54 2019
+//  Last Modified : <230323.1659>
+//
+//  Description	
+//
+//  Notes
+//
+//  History
+//	
+/////////////////////////////////////////////////////////////////////////////
+/// @copyright
+///    Copyright (C) 2019  Robert Heller D/B/A Deepwoods Software
+///			51 Locke Hill Road
+///			Wendell, MA 01379-9728
+///
+///    This program is free software; you can redistribute it and/or modify
+///    it under the terms of the GNU General Public License as published by
+///    the Free Software Foundation; either version 2 of the License, or
+///    (at your option) any later version.
+///
+///    This program is distributed in the hope that it will be useful,
+///    but WITHOUT ANY WARRANTY; without even the implied warranty of
+///    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+///    GNU General Public License for more details.
+///
+///    You should have received a copy of the GNU General Public License
+///    along with this program; if not, write to the Free Software
+///    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+/// @file Turnout.hxx
+/// @author Robert Heller 
+/// @date Sun Feb 24 14:51:54 2019 
+//////////////////////////////////////////////////////////////////////////////
 
 #ifndef _TURNOUT_HXX_
 #define _TURNOUT_HXX_

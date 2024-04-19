e07bd30a229bd926ef8ed941fb1f9295cb283382
Robert Heller <heller@deepsoft.com>
2024-04-19 10:50:19 -0400

    Completed first draft of Esp32SPI.

---
+++ ESP32-T7S3-TextDisplaySign/firmware/main/esp32textdisplay.cpp
@@ -0,0 +1,47 @@
+// -!- C++ -!- //////////////////////////////////////////////////////////////
+//
+//  System        : 
+//  Module        : 
+//  Object Name   : $RCSfile$
+//  Revision      : $Revision$
+//  Date          : $Date$
+//  Author        : $Author$
+//  Created By    : Robert Heller
+//  Created       : Wed Apr 17 10:16:07 2024
+//  Last Modified : <240417.1016>
+//
+//  Description	
+//
+//  Notes
+//
+//  History
+//	
+/////////////////////////////////////////////////////////////////////////////
+/// @copyright
+///    Copyright (C) 2024  Robert Heller D/B/A Deepwoods Software
+///			51 Locke Hill Road
+///			Wendell, MA 01379-9728
+///
+///    This program is free software; you can redistribute it and/or modify
+///    it under the terms of the GNU General Public License as published by
+///    the Free Software Foundation; either version 2 of the License, or
+///    (at your option) any later version.
+///
+///    This program is distributed in the hope that it will be useful,
+///    but WITHOUT ANY WARRANTY; without even the implied warranty of
+///    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+///    GNU General Public License for more details.
+///
+///    You should have received a copy of the GNU General Public License
+///    along with this program; if not, write to the Free Software
+///    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+/// @file esp32textdisplay.cpp
+/// @author Robert Heller
+/// @date Wed Apr 17 10:16:07 2024
+/// 
+///
+//////////////////////////////////////////////////////////////////////////////
+
+static const char rcsid[] = "@(#) : $Id$";
+
+

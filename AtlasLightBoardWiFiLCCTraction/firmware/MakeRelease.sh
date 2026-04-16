fa9b095450dbf20593ff23e78acbbd362bc599f5
Robert Heller <heller@deepsoft.com>
2026-04-16 13:14:05 -0400

    Upgrade ESP-IDF.

--- AtlasLightBoardWiFiLCCTraction/firmware/MakeRelease.sh
+++ AtlasLightBoardWiFiLCCTraction/firmware/MakeRelease.sh
@@ -17,7 +17,7 @@ export IDF_TARGET=esp32s3
 idf.py build
 BuildBinaries="build/bootloader/bootloader.bin build/partition_table/partition-table.bin build/ota_data_initial.bin build/AtlasLightBoardWiFiLCCTraction.bin"
 BuildDocs="doc/KitBooklet.pdf  doc/UserManual.pdf"
-FlashProgram='~/.espressif/python_env/idf5.0_py3.11_env/bin/python ~/esp-idf-v5.0.1/components/esptool_py/esptool/esptool.py -p (PORT) -b 460800 --before default_reset --after hard_reset --chip esp32s3  write_flash --flash_mode dio --flash_size 4MB --flash_freq 80m 0x0 bin/bootloader.bin 0x8000 bin/partition-table.bin 0xe000 bin/ota_data_initial.bin 0x10000 bin/AtlasLightBoardWiFiLCCTraction.bin'
+FlashProgram='~/.espressif/python_env/idf5.0_py3.11_env/bin/python ~/esp-idf-v5.4.3/esp-idf/components/esptool_py/esptool/esptool.py -p (PORT) -b 460800 --before default_reset --after hard_reset --chip esp32s3  write_flash --flash_mode dio --flash_size 4MB --flash_freq 80m 0x0 bin/bootloader.bin 0x8000 bin/partition-table.bin 0xe000 bin/ota_data_initial.bin 0x10000 bin/AtlasLightBoardWiFiLCCTraction.bin'
 GITRevShort=`git rev-parse --short HEAD`
 mkdir /tmp/AtlasLightBoardWiFiLCCTraction-$GITRevShort
 mkdir /tmp/AtlasLightBoardWiFiLCCTraction-$GITRevShort/bin
